da6b20e1953c8e06cb62f2f10469f834f328ab4f
wanaka <asd8116@gmail.com>
2019-08-08 13:53:32 +0800

    Install packges & add mixins

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <router-view />
   </div>
 </template>
 
